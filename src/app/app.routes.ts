c7a72d8aeccd4fd9df84a4eaf1f5224a40f82370
Nayelic98 <cajamarcanaye@gmail.com>
2025-11-07 23:42:46 -0500

    Cambios final

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
-import { HomePages } from './features/HomePages/HomePages';
+import { HomePage } from './features/HomePages/HomePages';
 import { PerfilPages } from './features/PerfilPages/PerfilPages';
 import { ProyectoPage } from './features/proyecto-page/proyecto-page';
 import { ProyectosDosPage } from './features/proyectos-dos-page/proyectos-dos-page';
 export const routes: Routes = [
-  { path: '', component: HomePages, title: 'Home' },
+  { path: '', component: HomePage, title: 'Home' },
   { path: 'perfil', component: PerfilPages, title: 'PerfilPages' },
   { path: 'proyecto', component: ProyectoPage, title: 'Proyecto' },
   { path: 'proyectodos', component: ProyectosDosPage, title: 'ProyectoDosPage' },
